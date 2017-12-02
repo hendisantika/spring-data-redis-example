f1b45ed472cdf34c6dbc5bc39793d25ef0e53d48
Hendi Santika <hendisantika@gmail.com>
2017-12-02 19:55:52 +0700

    #9 - Add Message Publisher Impl

---
+++ src/main/java/com/hendisantika/springdataredisexample/queue/MessagePublisherImpl.java
@@ -0,0 +1,39 @@
+package com.hendisantika.springdataredisexample.queue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-redis-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/12/17
+ * Time: 19.55
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public class MessagePublisherImpl implements MessagePublisher {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    
+    @Autowired
+    private ChannelTopic topic;
+
+    public MessagePublisherImpl() {
+    }
+
+    public MessagePublisherImpl(final RedisTemplate<String, Object> redisTemplate, final ChannelTopic topic) {
+        this.redisTemplate = redisTemplate;
+        this.topic = topic;
+    }
+
+    public void publish(final String message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+
+}
